153866453aed980b824ef99268137296a60d4f6c
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-03-22 23:13:50 +0600

    fundamentals

---
+++ fundamentals/Operators.py
@@ -0,0 +1,8 @@
+print(10 != 10)
+my_tax = 10.5
+your_tax = 10.5
+
+print(my_tax <= your_tax)
+
+my_marks = 30
+print(not(10 > 2 and 10 > 20 or 10 == 30))
---
+++ fundamentals/Variables.py
@@ -0,0 +1,9 @@
+my_name = 'test'
+your_name = 12345454545454545421218552
+
+age = 15
+tax = 10.5
+
+login_status = True
+
+print(age, tax, login_status)
